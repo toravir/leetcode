80243859510ff8bd0a704200ffca4c1814882735
Ravi Raju <ravir@employees.org>
2019-02-24 21:18:01 +0000

    tst

--- datastructures/fenwick.go
+++ datastructures/fenwick.go
@@ -1,1 +1,22 @@
-package 
+package main
+
+import (
+        "fmt"
+)
+
+type fenwickTree struct {
+    tree []int
+}
+
+func NewFenwickTree (inp []int) fenwickTree {
+    ft := fenwickTree{}
+    ft.tree = make([]int, 0, len(inp)+1)
+    ft.tree = append(ft.tree, 0)
+    ft.tree = append(ft.tree, inp...)
+    fmt.Println(ft)
+    return ft
+}
+
+func main () {
+    _ = NewFenwickTree([]int{1,2,3,4,5})
+}
